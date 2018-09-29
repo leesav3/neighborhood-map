4874d68b774d96cc6ad0342c972df4edb32aae43
Lee Savarese <lee.s@nc.rr.com>
2018-09-29 10:11:07 -0400

    added fetch to get JSON data (restaurants) from zomato

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TacoMap from './TacoMap';
 import './App.css';
 
 class App extends Component {
+  
 
   render() {
     return (
--- src/TacoMap.js
+++ src/TacoMap.js
@@ -4,8 +4,9 @@ import './App.css';
 class TacoMap extends Component {
 
 	componentDidMount() {
+		this.getVenues();
     	this.loadMap();
-  		}
+    }
 
   	loadMap = () => {
     	loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA3TEO3u-tQkhwwKu8TyI-RsmlQ_JwQ2Ho&callback=initMap");
@@ -14,10 +15,29 @@ class TacoMap extends Component {
   
   	initMap = () => {
     	const map = new window.google.maps.Map(document.getElementById('map'), {
-      		//center: {lat: -34.397, lng: 150.644},
       		center: {lat: 35.7796, lng: -78.6382},
       		zoom: 8
     	});
+  	}
+
+  	getVenues = () => {
+  		const apiURL = "https://developers.zomato.com/api/v2.1/search?entity_id=898&entity_type=city&count=10&cuisines=73&sort=rating";
+  		let venues = [];
+  		fetch(apiURL, {
+  			method: "GET",
+  			headers: new Headers ({
+  				'Content-Type' : 'application/json', 
+  				'user-key' : '75a828f3ff19fe64e68d40e776fd3da8'
+  			})
+  		}).then(response => {
+  			return response.json()
+  			}).then(json => {
+  				let venues = json.restaurants;
+  				console.log(json);
+  				console.log(venues[0].restaurant.name);
+  			}).catch(error => {
+  			console.log("Error: " + error);
+  		})
   	}
 
 	render() {
@@ -35,5 +55,8 @@ function loadScript(url) {
   script.defer = true
   index.parentNode.insertBefore(script, index)
 }
+
+
+
 
 export default TacoMap;
